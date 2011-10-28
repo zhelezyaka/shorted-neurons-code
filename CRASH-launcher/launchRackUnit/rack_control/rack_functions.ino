7445cc0e43f9bd8368fac5dfefbf72a152b79f9b
partially <partially@mad.scientist.com>
2011-10-28 02:19:17 +0000

    First basically functional controller and rack unit code

---
+++ CRASH-launcher/launchRackUnit/rack_control/rack_functions.ino
@@ -0,0 +1,48 @@
+boolean arm(void) {
+  if (status == status_safe) {
+    status = status_both_armed;
+    digitalWrite(safety_coil, HIGH);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+boolean disarm(void) {
+  digitalWrite(safety_coil, LOW);
+  if (!enter_orbit()) {
+    //error!
+  }
+  status = status_safe;
+  return true;
+  //add logic for dealing with comms success/failure
+}
+
+boolean engage(byte select_coils, byte warp_coils) {
+  byte coil_state = B00000000;
+  if (warp_coils == select_coils) {
+    if (status == status_both_armed) {
+      status = status_firing;
+      for (i = 0; i < sizeof(coil_select) - 1; i++) {
+        //this is only possible because the numeric value of
+        //pin #s 4-7 happen to match their place in the PORTD bitmap
+        bitWrite(coil_state, coil_select[i], bitRead(warp_coils, i));
+      }
+      PORTD |= coil_state;
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+boolean enter_orbit(void) {
+  PORTD &= coil_portd_alloff;
+  status = status_both_armed;
+  return true;
+  //add logic for dealing with comms success/failure
+}
+    
+    
