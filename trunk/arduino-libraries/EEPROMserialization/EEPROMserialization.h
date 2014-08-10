9313500510e0d301726612c244b05e100447e114
bts@bluefx.com <bts@bluefx.com@fd1a3f3f-7d8b-771b-4703-aaf75f57fbac>
2014-08-10 20:24:54 +0000

    do it damnit

---
+++ trunk/arduino-libraries/EEPROMserialization/EEPROMserialization.h
@@ -0,0 +1,18 @@
+#ifndef EEPROMserialization_h
+#define EEPROMserialization_h
+
+#include <EEPROM.h>
+#include <WProgram.h>
+
+/*
+template <class T> int EEPROM_writeAnything(int ee, const T& value);
+template <class T> int EEPROM_readAnything(int ee, T& value);
+*/
+
+/*
+int EEPROM_writeAnything(int ee, const T& value);
+int EEPROM_readAnything(int ee, T& value);
+*/
+
+
+#endif
